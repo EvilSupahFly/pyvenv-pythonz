854b2e53793962101283bf431fc8f0ed305b6875
EvilSupahFly <seann.giffin@gmail.com>
2024-10-15 18:57:45 -0400

    Update pyvenv.sh

--- pyvenv.sh
+++ pyvenv.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+check_src() {
+    # Check to see if the whole script is being sourced or running stand alone because this part shouldn't be included if sourced
+    if [[ "$(ps -o ppid= -p $$)" -eq 1 ]]; then
+        pyvenv $1 $2
+    fi
+}
+
+check_src $1 $2
+
+# Want automatic integreation? Copy everything below this line into either /etc/bash.bashrc or $HOME/.bashrc and restart your terminal!
+# Moving these colour-code variables outside this function makes them available to the whole script, not just this function
 RESET="\033[0m" #Normal
 RED="\033[1;31m" #Red
 GREEN="\033[1;32m" #Green
@@ -8,6 +19,9 @@ WHITE="\033[1;37m" #White
 
 # Function to check and create a Python virtual environment using pythonz
 pyvenv() {
+    # If no parameters are given, we prompt for both
+    # If only one is given, it will be assumed to be NAME
+    # If both are given, first will be assumed to be NAME, second will be VERSION
     local v_name=${1:-$(read -p "Enter the virtual environment name: " name && echo $name)}
     local v_home="$HOME/python-virtual-environs/$v_name"
 
@@ -27,24 +41,25 @@ pyvenv() {
         pyvenv $1
     fi
 
+    # Check to be sure user didn't just hit ENTER when prompted for NAME
     if [ -z "$v_name" ]; then
         echo -e "\n${RED}Error: Virtual environment name must be provided.\n"
-        return 1
+        exit 1
     fi
 
     if [ ! -d "$v_home" ]; then
         py_ver=${2:-$(read -p "Enter the Python version: " version && echo $version)}
 
         if [ ! pythonz list | grep -q "$py_ver" ]; then
-            echo -e "\n${RED}Python version '$py_ver' is not installed.\n${WHITE}Installing now.\n"
+            echo -e "\n${RED}Python version '${WHITE}$py_ver${RED}' is not installed.\n${WHITE}Installing now.\n"
             pythonz install "$py_ver"
         fi
 
-        echo -e "\n${YELLOW}Creating folder ${WHITE}$v_home${YELLOW} ...${RESET}\n"
+        echo -e "\n${WHITE}Creating folder ${GREEN}$v_home${RESET}\n"
         mkdir -p "$v_home"  # Create the directory for the virtual environment
         usepy="$(pythonz locate $py_ver)"
         if [ -z "$usepy" ]; then
-            echo -e "\n${RED}$usepy wasn't found, ${WHITE}despite supposedly being installed already.\nThat's both ${RED}unexpected ${WHITE}and ${RED}fatal${WHITE}. Gotta quit here.\n${RESET}"
+            echo -e "\n${RED}$usepy wasn't found, ${WHITE}despite supposedly being ${RED}already installed${WHITE}.\nThat's both ${RED}unexpected ${WHITE}and ${RED}fatal${WHITE}.\n${RESET}"
             exit 1
         fi
         echo -e "\n${WHITE}$usepy -m venv '$v_home'${RESET}"
